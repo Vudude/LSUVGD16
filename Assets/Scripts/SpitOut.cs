fb163b0748d2240322731174749d5c8bf4965ae4
Vu Le <vudude5o4@gmail.com>
2016-02-23 23:36:04 -0600

    Added projectile functionality that destroys enemy. Essentially a bullet until weapons are added.

---
+++ Assets/Scripts/SpitOut.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class SpitOut: MonoBehaviour {
+
+    public Rigidbody projectile;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Rigidbody clone;
+            clone = Instantiate(projectile, transform.position, transform.rotation) as Rigidbody;
+            clone.velocity = transform.TransformDirection(Vector3.forward * 10);
+        }
+	}
+}
