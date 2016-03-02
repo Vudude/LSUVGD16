83b41bf65e06afec50e560b3be9ef60cd8a195a1
bmautz <mautz.brandon@gmail.com>
2016-03-02 16:46:59 -0600

    GunModel Test Run
    Experimenting around with changing the models

---
+++ Assets/ModelGunSwitch.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ModelGunSwitch : MonoBehaviour 
+{
+
+	public Mesh replacementMesh;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag ("ability")) 
+		{
+			gameObject.GetComponent<MeshFilter> ().mesh = replacementMesh;
+		}
+	}
+}
