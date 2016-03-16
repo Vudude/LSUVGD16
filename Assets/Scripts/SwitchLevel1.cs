183ccecef33aaf80377927d6de79ee733df149ae
G0rg3th <tshoul2@lsu.edu>
2016-03-16 18:21:41 -0500

    Added SceneManagement and wallScript on covers

---
+++ Assets/Scripts/SwitchLevel1.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SwitchLevel1 : MonoBehaviour {
+
+    public void OnTrigger(Collider co)
+    {
+        if (co.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("level1.0");
+        }
+    }
+}
