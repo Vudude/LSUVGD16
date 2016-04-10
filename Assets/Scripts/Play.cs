a5ae1dbc22365b584ee55de133689415b9b847d5
bmautz <mautz.brandon@gmail.com>
2016-04-10 14:49:39 -0500

    made a start menu

---
+++ Assets/Scripts/Play.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Play : MonoBehaviour 
+{
+	public Canvas quitMenu;
+	public Button startText;
+	public Button exitText;
+
+	// Use this for initialization
+	void Start () 
+	{
+		quitMenu = quitMenu.GetComponent<Canvas>();
+		startText = startText.GetComponent<Button> ();
+		exitText = exitText.GetComponent<Button> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void StartLevel () //this function will be used on our Play button
+
+	{
+		SceneManager.LoadScene ("Scenes/Hub1.0"); //this will load our first level from our build settings. "1" is the second scene in our game
+
+	}
+
+	public void ExitGame () //This function will be used on our "Exit" button 
+
+	{
+		Application.Quit(); //this will quit our game. Note this will only work after building the game
+
+	}
+}		
