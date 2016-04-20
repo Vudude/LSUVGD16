5cf82b2eec6719d0f07b84b3db392258096a0682
Vu Le <vudude5o4@gmail.com>
2016-04-19 21:43:19 -0500

    Add spawn enemies. Add rotate to player and shoot.

--- Assets/Scripts/BossAI.cs
+++ Assets/Scripts/BossAI.cs
@@ -4,49 +4,62 @@ using System.Collections;
 public class BossAI : MonoBehaviour {
 
 	public float gunTimer = 2f;
-	public GameObject enemySpawn;
+    public float spawnTimer = 5;
+    public GameObject[] spawnPoints;
 	public Rigidbody projectile;
+    public GameObject enemyPrefab;
+    public GameObject player;
 
-	private float shootTimer;
+    public float rotationSpeed;
+
+
+    private Quaternion _lookRotation;
+    private Vector3 _direction;
+    public float shootTimer;
+    public float spawnTiming;
+    public float projectileSpeed = 500;
+
 	// Use this for initialization
 	void Start () {
 
 		shootTimer = gunTimer;
+        spawnTiming = spawnTimer;
+        spawnPoints = GameObject.FindGameObjectsWithTag("spawnPoint");
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		shootTimer -= Time.deltaTime;
+		spawnTiming -= Time.deltaTime;
 
+        if (shootTimer <= 0)
+            Shoot();
 
-		ShootCircle();
+        //look at player
+        _direction = (player.transform.position - transform.position).normalized;
+        Debug.Log(player.transform.position);
+        _lookRotation = Quaternion.LookRotation(_direction);
+        transform.rotation = Quaternion.Slerp(transform.rotation, _lookRotation, Time.deltaTime * rotationSpeed);
 	
+        if (spawnTiming <= 0)
+        {
+            int random = (int) Random.value * 100 % spawnPoints.Length;
+            SpawnEnemy(spawnPoints[random].transform);
+        }
+        
 	}
 
-	private void ShootCircle() {
-		Shoot(transform.forward);
-		Shoot(-transform.forward);
-		Shoot(-transform.right);
-		Shoot(transform.right);
-		Shoot(transform.forward * .25);
-		Shoot(-transform.forward * .25);
-		Shoot(-transform.right * .25);
-		Shoot(transform.right + .25);
-            	shootTimer = gunTimer;
-	}
-
-	private void Shoot(Vector3 direction) {
+	private void Shoot() {
 		Rigidbody clone;
-            	clone = Instantiate(projectile, transform.position + direction + Vector3.up, transform.rotation) as Rigidbody;
-
+            	clone = Instantiate(projectile, transform.position + transform.forward + Vector3.up, transform.rotation) as Rigidbody;
             	clone.AddForce(clone.transform.forward * projectileSpeed);
+        shootTimer = gunTimer;
 	}
 
-	private void SpawnEnemy(Transform[] spawnPositions) {
-		for (int i = 0; i < spawnPositions.length; i++) {
-			GameObject enemy;
-			enemy = Instantiate(enemySpawn, spawnPositions[i].position, spawnPositions.rotation) as GameObject;
-		}
+	private void SpawnEnemy(Transform spawn) {
+		GameObject enemy;
+        enemy = Instantiate(enemyPrefab, spawn.position, spawn.rotation) as GameObject;
+        spawnTiming = spawnTimer;
 	}
 }
