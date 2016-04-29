fbb08bbfcc8cd1decb3a145908eeca943e64daf7
bmautz <mautz.brandon@gmail.com>
2016-04-28 23:19:15 -0500

    added sound for boss

---
+++ Assets/PlayAgain.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PlayAgain : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	public void playAgain()
+	{ 
+		SceneManager.LoadScene("Scenes/tutorial2");
+	}
+}
